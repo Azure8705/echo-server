c05bbee761968cc33d599318d089dea05a191bc3
Azure <kvnliwag@gmail.com>
2015-08-23 12:51:33 -0700

    Fix

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response) {
+   response.json({
+      name: 'Kim Gordon',
+      instrument: 'Bass' 
+   }); 
+});
+
+app.listen(8080);
